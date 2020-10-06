2111ebb3b6e3dd9747bc6030175330aa524e453e
Pierre Lundström <pierre.lundstrom@learnet.se>
2020-10-06 13:40:20 +0200

    updated

--- src/Evolution.java
+++ src/Evolution.java
@@ -1,11 +1,44 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Evolution extends Canvas implements Runnable {
+    JFrame frame;
+    BufferedImage image;
+    int[] pixels;
+    int fps;
+    int ups;
+    int scale;
+    double t = 0;
     boolean running;
     Thread thread;
-    ArrayList<Cell> cells = new ArrayList<Cell>();
+    ArrayList<Cell> cells;
+    Grid grid;
+    int width;
+    int height;
+
+    public Evolution(Grid grid, int w, int h, int scale){
+        this.grid = grid;
+        this.width = w;
+        this.height = h;
+        image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        for (int i = 0 ; i < pixels.length ; i++) {
+            pixels[i] =;
+        }
+       // initComponents();
+
+    }
+
+
+
+
 
     public synchronized void start() {
         running = true;
@@ -58,29 +91,41 @@ public class Evolution extends Canvas implements Runnable {
 
         @Override
         public void run () {
-            double frameUpdateinteval = 1000000000.0 / fps;
-            double stateUpdateinteval = 1000000000.0 / ups;
-            double deltaFrame = 0;
-            double deltaUpdate = 0;
-            long lastTime = System.nanoTime();
 
             while (running) {
                 long now = System.nanoTime();
-                Cell cell = new Cell();
-                deltaFrame += (now - lastTime) / frameUpdateinteval;
-                deltaUpdate += (now - lastTime) / stateUpdateinteval;
-                lastTime = now;
+                Random rand = new Random();
+                Cell cell = new Cell(rand.nextInt(), rand.nextInt());
+                Cell neighbor = new Cell(rand.nextInt(), rand.nextInt());
+                cell.addNeighbor(neighbor);
+                if (Math.random() < 0.5){
+                    Cell neighbor2 = new Cell(rand.nextInt(), rand.nextInt());
+                    Cell neighbor3 = new Cell(rand.nextInt(), rand.nextInt());
+                    cell.addNeighbor(neighbor2);
+                    cell.addNeighbor(neighbor3);
+                }
+                grid.getCells().add(cell);
+                cells = grid.getCells();
 
-                while (deltaUpdate >= 1) {
-                    update();
-                    deltaUpdate--;
+                for (int i = 0; i < cells.size(); i++){
+                    if (cells.get(i).getNeighbors().size() < 2){
+                        if (cells.get(i).getCellAlive()) {
+                            cells.get(i).setCellAlive(false);
+                            System.out.println("killed cell because cell < 2" + cells.get(i).toString());
+                        }
+                    } else if (cells.get(i).getNeighbors().size() == 3){
+                        if (!cells.get(i).getCellAlive()) {
+                            cells.get(i).setCellAlive(true);
+                            System.out.println("cell is alive: " + cells.get(i).toString());
+                        }
+                    }else{
+                        if (cells.get(i).getCellAlive()) {
+                            cells.get(i).setCellAlive(false);
+                            System.out.println("killed cell because size > 3" + cells.get(i).toString());
+                        }
+                    }
                 }
 
-                while (deltaFrame >= 1) {
-                    draw();
-                    deltaFrame--;
-                }
-            }
             //stop();
         }
     }
--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GameOfLife {
+
     public static void main(String[]args) {
-        Evolution evolutionManager = new Evolution();
-        evolutionManager.start();
+        Grid grid = new Grid(10, 10);
+            Evolution evolutionManager = new Evolution(grid);
+            evolutionManager.start();
+
+        /*Cell cell1 = new Cell(4, 4, true);
+        Cell cell2 = new Cell(10, 10, false);
+        Cell cell3 = new Cell(12, 12, false);
+        Cell cell4 = new Cell(13, 13, false);
+        Cell cell5 = new Cell(14, 14, false);
+        Cell cell6 = new Cell(15, 15, false);
+        Cell cell7 = new Cell(16,16, false);
+        Cell cell8 = new Cell(17, 17, false);
+        cell1.addNeighbor(cell2);
+        cell2.addNeighbor(cell1);
+        cell2.addNeighbor(cell3);
+        cell2.addNeighbor(cell4);
+        System.out.println(cell2.getNeighbors().toString());*/
+        /*Grid grid = new Grid(10, 10);
+        grid.cells.add(cell1);
+        grid.cells.add(cell2);
+        grid.cells.add(cell3);
+        grid.cells.add(cell4);
+        grid.cells.add(cell5);
+        grid.cells.add(cell6);
+        grid.cells.add(cell7);
+        grid.cells.add(cell8);*/
         //generate cells
         //kill if cell has less than 2 neighbors
         //live if cell has exactly 3 neighbors
